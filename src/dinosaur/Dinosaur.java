3d74ab8614df9e0418aa5fcd25a856785655e1bc
Tyler-Cav <tyler.cavanaugh24@gmail.com>
2024-11-18 18:31:42 -0500

    Updated Main and added Dinosaur package.

---
+++ src/dinosaur/Dinosaur.java
@@ -0,0 +1,22 @@
+package dinosaur;
+
+public class Dinosaur {
+    private String id;
+
+    public Dinosaur(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDino() {
+        return "Dinosaur";
+    }
+
+}
